915ca1386e5e234dda8c36a8740aca91e6f7801c
Asachuna <84565188+yugoka@users.noreply.github.com>
2024-04-10 18:21:44 +0900

    読み込み速度、スタイリングなど改善

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const checkinId = getCookie("sessionToken"); // CookieからcheckinIdを取得
 
+  // チェックイン/チェックアウトボタンを初期状態では非表示に設定
+  const checkinButton = document.querySelector("button[onclick*='checkin']");
+  const checkoutButton = document.querySelector("button[onclick*='checkout']");
+  checkinButton.style.display = "none";
+  checkoutButton.style.display = "none";
+
   fetch(
     "https://script.google.com/macros/s/AKfycbyULbKzai6KYhNbPm9uQbwin3ylgMvzvxS-MhZsUCcaPRI0MJickCsS7Eb9rI0fizuF/exec" +
       "?checkinId=" +
@@ -13,16 +19,31 @@ document.addEventListener("DOMContentLoaded", function () {
         ? `部室に${data.count}人います`
         : "部室に人は居ません";
 
-      // ユーザーが部屋にいるかどうかを表示
+      // ローディングアニメーションを削除
+      const spinner = document.querySelector(".spinner");
+      if (spinner) {
+        spinner.remove();
+      }
+
+      // ユーザーが部屋にいるかどうかを表示し、対応するボタンを表示
       if (data.isUserInRoom) {
         document.getElementById("user-status").textContent =
           "あなたは現在部室にいます";
+        checkoutButton.style.display = "block"; // ユーザーが部屋にいればチェックアウトボタンを表示
       } else {
         document.getElementById("user-status").textContent =
           "あなたは現在部室にいません";
+        checkinButton.style.display = "block"; // ユーザーが部屋にいなければチェックインボタンを表示
       }
     })
-    .catch((error) => console.error("Error:", error));
+    .catch((error) => {
+      console.error("Error:", error);
+      // エラーが発生した場合もローディングアニメーションを削除
+      const spinner = document.querySelector(".spinner");
+      if (spinner) {
+        spinner.remove();
+      }
+    });
 });
 
 function getCookie(name) {
